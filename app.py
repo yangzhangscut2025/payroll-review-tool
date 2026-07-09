92e7feca0a717e28f172a478b37350a9ff5c5748
纸鸢扬021 <13284412+zhiyuan-yang-021@user.noreply.gitee.com>
2026-07-09 09:44:04 +0800

    恢复端口5000

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=5001, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
